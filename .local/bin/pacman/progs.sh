ce555c44d1b45bdf513faba6073914023272bc2f
Mantas Sarauskas <sarauskas.mantas@gmail.com>
2020-08-15 12:19:31 +0300

    Improve i3 statusbar

--- .local/bin/pacman/progs.sh
+++ .local/bin/pacman/progs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
-sudo pacman -S zsh zsh-autosuggestions openfortivpn mpv ranger neovim atool atom arduino arduino-docs ardour alsa-utils acpi zsh-completions zathura zathura-djvu zathura-pdf-mupdf youtube-dl xorg-xprop xorg-server wget unzip unrar ueberzug tree tmux tldr supercollider sc3-plugins rsync r qutebrowser ctags docker docker-compose dunst faust ffmpeg foxdot fzf git gnustep-base gnustep-make haskell-tidal i3-gaps i3blocks ipython jack2 japa libnotify libreoffice-still lsof luarocks lv2 mpv neofetch nginx pandoc pandoc-citeproc pulseaudio pulseaudio-alsa pulseaudio-jack pavucontrol python-i3ipc python-pynvim python-pywal ripgrep ttf-ubuntu-font-family unclutter scrot redshift termite sxiv feh picom networkmanager pulsemixer xdotool rofi mpc mpd ncmpcpp transmission-cli transmission-gtk pass
+sudo pacman -S zsh zsh-autosuggestions openfortivpn mpv ranger neovim atool atom arduino arduino-docs ardour alsa-utils acpi zsh-completions zathura zathura-djvu zathura-pdf-mupdf youtube-dl xorg-xprop xorg-server wget unzip unrar ueberzug tree tmux tldr supercollider sc3-plugins rsync r qutebrowser ctags docker docker-compose dunst faust ffmpeg foxdot fzf git gnustep-base gnustep-make haskell-tidal i3-gaps i3blocks ipython jack2 japa libnotify libreoffice-still lsof luarocks lv2 mpv neofetch nginx pandoc pandoc-citeproc pulseaudio pulseaudio-alsa pulseaudio-jack pavucontrol python-i3ipc python-pynvim python-pywal ripgrep ttf-ubuntu-font-family unclutter scrot redshift termite sxiv feh picom networkmanager pulsemixer xdotool rofi mpc mpd ncmpcpp transmission-cli transmission-gtk pass syncthing firefox ttf-nerd-fonts-symbols-mono
 
-yay -S zsh-plugin-wd-git zsh-fast-syntax-highlighting-git navi pyradio-git shiny-server-git mongodb-bin mongodb-tools-bin neovide rstudio-desktop-bin faustlive-git htop-vim-git nerd-fonts-hack xcwd-git
+# install yay package manually
+
+yay -S zsh-plugin-wd-git zsh-fast-syntax-highlighting-git navi pyradio-git shiny-server-git mongodb-bin mongodb-tools-bin neovide rstudio-desktop-bin faustlive-git htop-vim-git xcwd-git
 
 # https://github.com/mantsar/pkgbuilds cadence-claudia
