14c9fcf38b9e729b2aaa80907225c78501724afa
Mantas Sarauskas <sarauskas.mantas@gmail.com>
2020-08-21 15:08:08 +0300

    Improve sp init.sh

--- sp/init.sh
+++ sp/init.sh
@@ -1,6 +1,26 @@
+#!/bin/bash
+
 git clone https://github.com/tidalcycles/Dirt-Samples
 
 mkdir ardour
 
+git clone https://github.com/k-takata/minpac.git \
+    ~/.config/nvim/pack/minpac/opt/minpac
+# :updateRemotePlugins
+
+gpasswd -a $USER realtime
+gpasswd -a $USER audio
+
+# Select in cadence gui to spawn on autologin and remove spawning for pulse
+/etc/pulse/client.cont
+# uncomment
+autospawn = no
+; autospawn = yes
+
+# Fix ardour warning and real time scheduling:
+# https://wiki.debian.org/Ardour
+# https://jackaudio.org/faq/linux_rt_config.html
+
+# Tidal completion sources:
 # https://tidalcycles.org/index.php/All_the_functions # copy into file then:
 # https://github.com/tidalcycles/Tidal/blob/main/src/Sound/Tidal/Chords.hs#L113
