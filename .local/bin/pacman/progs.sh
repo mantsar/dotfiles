a8a1bc4e787a6d170360fd01aff003da9b842c2a
Mantas <sarauskas.mantas@gmail.com>
2020-08-22 22:09:52 +0300

    Improve installation script

--- .local/bin/pacman/progs.sh
+++ .local/bin/pacman/progs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo pacman -S zsh zsh-autosuggestions openfortivpn mpv ranger neovim atool atom ardour alsa-utils acpi zsh-completions zathura zathura-djvu zathura-pdf-mupdf youtube-dl xorg-xprop xorg-server wget unzip unrar ueberzug tree tmux tldr supercollider sc3-plugins rsync r qutebrowser ctags docker docker-compose dunst faust ffmpeg foxdot fzf git gnustep-base gnustep-make haskell-tidal i3-gaps i3blocks ipython jack2 japa libnotify libreoffice-still lsof luarocks lv2 mpv neofetch nginx pandoc pandoc-citeproc pulseaudio pulseaudio-alsa pulseaudio-jack pavucontrol python-i3ipc python-pynvim python-pywal ripgrep ttf-ubuntu-font-family unclutter scrot redshift termite sxiv feh picom networkmanager pulsemixer xdotool rofi mpc mpd ncmpcpp transmission-cli transmission-gtk pass syncthing pd r xclip npm python-dbus xorg-xinit
+sudo pacman -S zsh zsh-autosuggestions openfortivpn mpv ranger neovim atool atom ardour alsa-utils acpi zsh-completions zathura zathura-djvu zathura-pdf-mupdf youtube-dl xorg-xprop xorg-server wget unzip unrar ueberzug tree tmux tldr supercollider sc3-plugins rsync r qutebrowser ctags docker docker-compose dunst faust ffmpeg foxdot fzf git gnustep-base gnustep-make haskell-tidal i3-gaps i3blocks ipython jack2 japa libnotify libreoffice-still lsof luarocks lv2 mpv neofetch nginx pandoc pandoc-citeproc pulseaudio pulseaudio-alsa pulseaudio-jack pavucontrol python-i3ipc python-pynvim python-pywal ripgrep ttf-ubuntu-font-family unclutter scrot redshift termite sxiv feh picom networkmanager pulsemixer xdotool rofi mpc mpd ncmpcpp transmission-cli transmission-gtk pass syncthing pd r xclip npm python-dbus xorg-xinit ttf-nerd-fonts-symbols-mono ttf-linux-libertine ttf-joypixels xorg-xwininfo dmenu python-tldextract
 
 mkdir $HOME/Downloads $HOME/tmp $HOME/Videos $HOME/Pictures
 
@@ -9,20 +9,18 @@ git clone https://www.github.com/mantsar/pkgbuilds.git $HOME/pkgbuilds
 cd $HOME/pkgbuilds/cadence-claudia
 git remote set-url origin ssh://git@github.com/mantsar/pkgbuilds.git
 makepkg -si
-
 cd ..
-
+git remote set-url origin ssh://git@github.com/mantsar/pkgbuilds.git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -S zsh-plugin-wd-git zsh-fast-syntax-highlighting-git navi pyradio-git rstudio-desktop-bin faustlive-git htop-vim-git xcwd-git ladish python2-distutils-extra
+yay -S zsh-plugin-wd-git zsh-fast-syntax-highlighting-git navi pyradio-git rstudio-desktop-bin faustlive-git htop-vim-git xcwd-git ladish python2-distutils-extra nerd-fonts-hack
 
 # shiny-server-git
 # mongodb-bin
 # mongodb-tools-bin
 # firefox
-# ttf-nerd-fonts-symbols-mono
 # arduino
 # arduino-docs
 
@@ -35,4 +33,12 @@ git clone https://github.com/k-takata/minpac.git \
 git config --global user.email "sarauskas.mantas@gmail.com"
 git config --global user.name Mantas
 
+git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+# echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
+
+source $HOME/.zshrc
+
+mkdir $HOME/.cache/zsh && touch $HOME/.cache/zsh/history
+
 d remote set-url origin ssh://git@github.com/mantsar/dotfiles.git
+d config --local status.showUntrackedFiles no
