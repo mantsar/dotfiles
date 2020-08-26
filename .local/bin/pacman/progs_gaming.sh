29c6172225e25cd2a5e82e0de3f2cc566b1d2b40
Mantas <sarauskas.mantas@gmail.com>
2020-08-26 16:11:18 +0000

    Minor fixes

--- .local/bin/pacman/progs_gaming.sh
+++ .local/bin/pacman/progs_gaming.sh
@@ -6,3 +6,4 @@ sudo pacman -S steam discord lutris
 # https://github.com/lutris/lutris/wiki
 # install proton from within steam
 # Remove Steam ads > setting > interface > uncheck "Notify me about additions or changes to my games..."
+# Shader Pre-Caching > check "Allow background processing of Vulkan shaders"
