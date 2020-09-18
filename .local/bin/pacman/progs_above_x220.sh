eebd9a5d729c70b46b19229b6277b6d2101acc70
Mantas <sarauskas.mantas@gmail.com>
2020-09-18 13:29:38 +0300

    Add bionfoprogs

--- .local/bin/pacman/progs_above_x220.sh
+++ .local/bin/pacman/progs_above_x220.sh
@@ -8,7 +8,7 @@ sudo pacman -S gcc-fortran unixodbc gnu-free-fonts
 gpg --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 --recv 27EDEAF22F3ABCEB50DB9A125CC908FDB71E12C2
 sudo pacman -Syy
 
-yay -S neovide ttf-fira-code slack-desktop mongodb-bin mongodb-tools-bin rstudio-desktop-bin
+yay -S neovide ttf-fira-code slack-desktop mongodb-bin mongodb-tools-bin rstudio-desktop-bin htslib # provides bgzip/tabix
 
 # shiny-server-git
 Rscript progs_r.R
